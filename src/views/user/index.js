cee13674f9854dc44027977ace2688bdab4e6512
CodeOfFire <virensingh1906@gmail.com>
2024-10-29 17:32:18 +0530

    wip code

--- src/views/user/index.js
+++ src/views/user/index.js
@@ -1,23 +1,11 @@
 import React, { useState } from "react";
 import FormCollection from "../../components/FormCollection";
 import Button from "../../components/Button";
-import { INITIAL_STATE, } from "../../scripts/formData";
-
-
-
-
-
+import { INITIAL_STATE } from "../../scripts/formData";
 
 const User = () => {
-[setUserDetails,setUserDetails] = useState(INITIAL_STATE)
-  return (
-    <div>
-      
-
-      
-    </div>
-  );
+  const [userDetails, setUserDetails] = useState(INITIAL_STATE);
+  return <div></div>;
 };
 
 export default User;
-
